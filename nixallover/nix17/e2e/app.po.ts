d3b0d0d226df199e9631a1b5dbc57caec68cfae0
nixallover <nicole.oliver.42@gmail.com>
2017-02-27 17:09:29 -0800

    started new angular proj

---
+++ nixallover/nix17/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Nix17Page {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
