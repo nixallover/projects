e99d315f063e38b05150507367a063bcd2c00de8
nixallover <nicole.oliver.42@gmail.com>
2017-03-02 12:01:47 -0800

    created basic contact links and added custom icon font set

--- nixallover/nix17/src/app/app.module.ts
+++ nixallover/nix17/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SkillsComponent } from './skills/skills.component';
 import { SectionComponent } from './section/section.component';
 import { TitleComponent } from './title/title.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
     SectionComponent,
-    TitleComponent
+    TitleComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
