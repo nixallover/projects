c0171ad1e22f972b8c3d431a1c06faa6b123e5e9
nixallover <nicole.oliver.42@gmail.com>
2017-03-01 19:26:28 -0800

    created section component and added the major sections to it

--- nixallover/nix17/src/app/app.module.ts
+++ nixallover/nix17/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SkillsComponent } from './skills/skills.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsComponent
+    SkillsComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
