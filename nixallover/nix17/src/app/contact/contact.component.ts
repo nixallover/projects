f4b52ba5f969d288ecedcc7705cf98ee30a2f157
nixallover <nicole.oliver.42@gmail.com>
2017-03-02 17:15:13 -0800

    created first pass social link animation

--- nixallover/nix17/src/app/contact/contact.component.ts
+++ nixallover/nix17/src/app/contact/contact.component.ts
@@ -6,6 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  private contactLinks: any[] = [
+    {
+      title: 'E-mail',
+      url: 'mailto:nicole@nixallover.com',
+      icon: 'envelope'
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/nicolesoliver',
+      icon: 'linkedin'
+    },
+    {
+      title: 'Twitter',
+      url: 'https://twitter.com/nixallover',
+      icon: 'twitter'
+    },
+    {
+      title: 'Pinterest',
+      url: 'https://www.pinterest.com/nixallover/',
+      icon: 'pinterest'
+    },
+    {
+      title: 'GitHub',
+      url: 'https://github.com/nixallover',
+      icon: 'github'
+    },
+    {
+      title: 'JS Fiddle',
+      url: 'http://jsfiddle.net/user/nixallover/fiddles/',
+      icon: 'jsfiddle'
+    }
+  ];
 
   constructor() { }
 
