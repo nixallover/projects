03350510cf214b423ec358dc0d329d21b740c5c4
nixallover <nicole.oliver.42@gmail.com>
2017-09-06 19:11:12 -0700

    added a title animation and some other superficial style changes

--- nixallover/2017-08-26/src/app/presentations/presentations.component.ts
+++ nixallover/2017-08-26/src/app/presentations/presentations.component.ts
@@ -12,6 +12,7 @@ export class PresentationsComponent implements OnInit {
       subtitle: 'UC San Diego Campus Lisa Talk',
       date: 'July 2015',
       url: 'http://slides.com/nixallover/nodejs',
+      linkText: 'Browse the slides',
       description: 'I co-presented this one hour intro to Node.js, discussing ways that our shop has utilized it to create internal tooling'
     },
     {
@@ -19,6 +20,7 @@ export class PresentationsComponent implements OnInit {
       subtitle: 'Adobe Illustrator Tutorial',
       date: 'August 2012',
       url: 'https://www.youtube.com/watch?v=5W3SZ_o8m5o',
+      linkText: 'Watch the tutorial',
       description: 'A screencast describing how to create a custom text envelope in Illustrator. Runner up in Deke Techniques: the Challenge (Illustrator category)'
     }
   ];
