1e962994df74aef780205f4df8265e396f00842e
nixallover <nicole.oliver.42@gmail.com>
2017-10-18 20:28:22 -0700

    fleshed out modal styles more

--- nixallover/2017-08-26/src/app/projects/projects.component.ts
+++ nixallover/2017-08-26/src/app/projects/projects.component.ts
@@ -6,6 +6,7 @@ class Project {
   date: string;
   image: string;
   url: string;
+  roleDescription: string;
   description: string;
   technologies: string[];
 }
@@ -23,6 +24,7 @@ export class ProjectsComponent implements OnInit {
       date: '2016-present',
       image: 'isa',
       url: null,
+      roleDescription: 'Front-end lead developer. Developed interfaces from prototype to QA.',
       description: 'A UC San Diego campus-wide application that allow schedulers to plan course schedules for the year, review instructors for their unit, communicate with instructors about their teaching preferences, and generate reports about instruction',
       technologies: ['Node.js', 'Typescript', 'Angular', 'Sass']
     },
@@ -32,6 +34,7 @@ export class ProjectsComponent implements OnInit {
       date: '2015-16',
       image: 'lab',
       url: null,
+      roleDescription: 'Full-stack developer. Designed front and back end.',
       description: '',
       technologies: ['Node.js', 'Express.js', 'Sass', 'Jade (Pug)']
     },
@@ -41,6 +44,7 @@ export class ProjectsComponent implements OnInit {
       date: 'June 2015',
       image: 'zmd',
       url: 'http://zombiematchdefense.com/',
+      roleDescription: 'Worked with client to design a dynamic website.',
       description: 'A promotional website for a mobile game featuring parallax scrolling and CSS3 animations',
       technologies: ['Sass', 'Skrollr.js']
     },
@@ -50,6 +54,7 @@ export class ProjectsComponent implements OnInit {
       date: '2014-15',
       image: 'amb',
       url: null,
+      roleDescription: 'Surveyed development team to identify front end toolset needs.',
       description: '',
       technologies: ['Node.js', 'Sass', 'Grunt', 'JQuery', 'Bootstrap']
     }
